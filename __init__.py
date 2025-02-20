883075331716a254dea13b9f677f412afd7518a9
小萌新 <3209970865@qq.com>
2025-02-20 16:36:32 +0800

    Feat: modify version 1.2.3 -> 1.2.4

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from . import ui, ops, res, preferences
 bl_info = {
     "name": "Lattice Helper",
     "author": "AIGODLIKE Community: 会飞的键盘侠,小萌新",
-    "version": (1, 2, 3),
+    "version": (1, 2, 4),
     "blender": (2, 90, 0),
     "location": "3DView -> Context Menu -> LatticeHelper",
     "description": "Apply Lattice modifier to object(s) in object/edit mode",

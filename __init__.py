75493d703653e5a5c99e0697a0d16bfe6354c2ce
小萌新 <3209970865@qq.com>
2025-02-20 15:38:02 +0800

    Feat: update version 1.2.2 -> 1.2.3

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from . import ui, ops, res, preferences
 bl_info = {
     "name": "Lattice Helper",
     "author": "AIGODLIKE Community: 会飞的键盘侠,小萌新",
-    "version": (1, 2, 2),
+    "version": (1, 2, 3),
     "blender": (2, 90, 0),
     "location": "3DView -> Context Menu -> LatticeHelper",
     "description": "Add matching lattices to a single or multiple objects in object or editing mode",
